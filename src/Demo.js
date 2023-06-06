35d60a871652153dbe2a716dea68062483f1a145
Abdul quddos <abdulquddoshafiz@gmail.com>
2023-06-06 06:08:37 +0500

    Demo

---
+++ src/Demo.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Demo.css';
+const Demo = () => {
+  return (
+    <div>
+        <div className="elementor-widget-container">
+            <h2 className="elementor-heading-title">We believe be possible to offer a high level <br/> solution for all company sizes.
+                 Easy to install and <br/> operate at affordable investment !</h2>
+        <div className="elementor-button-container">
+            <a href="#" className='elementor-button-link'>
+              <span className='elementor-button-wrapper'>
+                <span className='elementor-button-text'>Request a Demo</span>
+              </span>
+              </a>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default Demo
